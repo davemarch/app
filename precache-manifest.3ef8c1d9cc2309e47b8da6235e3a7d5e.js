f7faf55d0f1a8f8f19771c73edf6056f78862b4f
davemarch <davemarchaudio@gmail.com>
2020-09-06 15:00:02 +0100

    Updates

---
+++ precache-manifest.3ef8c1d9cc2309e47b8da6235e3a7d5e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e2925bd2f79ced7d462afb871b01aba",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "b7fca45e380b4c4db3fb",
+    "url": "/app/static/css/main.aa339c7b.chunk.css"
+  },
+  {
+    "revision": "87719e336450423e1622",
+    "url": "/app/static/js/2.ac677fbf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app/static/js/2.ac677fbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7fca45e380b4c4db3fb",
+    "url": "/app/static/js/main.7515b2fc.chunk.js"
+  },
+  {
+    "revision": "aca9cec55175d18095b8",
+    "url": "/app/static/js/runtime-main.d0c462e8.js"
+  },
+  {
+    "revision": "28ab29395567722a1065c4be7d1040f1",
+    "url": "/app/static/media/jan2010_USA_poster.28ab2939.jpg"
+  },
+  {
+    "revision": "9fdf50770589cec42b6e1f89e988f371",
+    "url": "/app/static/media/jan2020_EUROPE_poster.9fdf5077.jpg"
+  },
+  {
+    "revision": "c2de8131ebe533eae38f8291140098cd",
+    "url": "/app/static/media/logo.c2de8131.png"
+  },
+  {
+    "revision": "1bbce4a48d98b54481a8bcd067940c53",
+    "url": "/app/static/media/nov2010_UK_poster.1bbce4a4.png"
+  },
+  {
+    "revision": "633d3d6a27154ad5e64949cc6eb2265c",
+    "url": "/app/static/media/sept2010_UK_poster.633d3d6a.jpg"
+  }
+]);
